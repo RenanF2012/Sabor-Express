d258b592ccbacaa15fa33ce4cd8d9e3fa12c85da
RenanF2012 <132415076+RenanF2012@users.noreply.github.com>
2024-07-05 00:25:43 -0300

    Update app.py

--- SABOR-EXPRESS/app.py
+++ SABOR-EXPRESS/app.py
@@ -1,3 +1,5 @@
+import os
+
 print("""
       
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -15,6 +17,10 @@ print('4. Sair\n')
 
 opcao_escolhida = int(input('Escolha uma opção: '))
 
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando o app\n')
+
 if opcao_escolhida == 1:
     print('Cadastrar restaurante')
 elif opcao_escolhida == 2:
@@ -22,4 +28,4 @@ elif opcao_escolhida == 2:
 elif opcao_escolhida == 3:
     print('Ativar restaurante')
 else:
-    print('Encerrando o programa')  
+    finalizar_app() 
